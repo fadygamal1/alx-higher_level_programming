31ce2ec4f18c838fc0fead7ebfdd1f4f062b7ef5
fadygamal11 <fadygamal007@gmail.com>
2023-11-28 06:31:00 -0800

    commit

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for x in str:
+        y = ord(x)
+        if y >= 97 and y <= 122:
+            y -= 32
+        print("{}".format(chr(y)), end='')
+    print()
