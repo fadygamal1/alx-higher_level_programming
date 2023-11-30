c694f7b2862547797742ec67bc1337b881cce794
fadygamal11 <fadygamal007@gmail.com>
2023-11-30 06:29:51 -0800

    commit

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ == __main__:
-    from add_0 import add
+
+from add_0 import add
+
+if __name__ == "__main__":
+
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    sum = add(a, b)
+
+    print("{0:d} + {1:d} = {2:d}".format(a, b, sum))
